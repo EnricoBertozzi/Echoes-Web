182f0a639182d83c25119a3314fcb5989dfe0e30
Avraham <souzaavraham@proton.me>
2026-09-10 17:38:47 -0300

    feat: initial table

--- app/pages/dashboard/users/index.tsx
+++ app/pages/dashboard/users/index.tsx
@@ -1,5 +1,16 @@
 export default function Devices() {
   return (
-    <main className='flex flex-1'></main>
+    <main className='flex flex-1 justify-center items-center'>
+      <table>
+        <thead className='bg-main rounded'>
+          <tr>
+            <td className='py-2'>Nome</td>
+            <td>E-mail</td>
+            <td>Função</td>
+            <td>Ações</td>
+          </tr>
+        </thead>
+      </table>
+    </main>
   )
 }
