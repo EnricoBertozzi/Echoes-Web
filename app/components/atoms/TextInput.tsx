56e7df071fdbcab4bef54c4b440ee82e6bec996f
Avraham <souzaavraham@proton.me>
2026-09-10 16:01:42 -0300

    fix: input and title styles

--- app/components/atoms/TextInput.tsx
+++ app/components/atoms/TextInput.tsx
@@ -6,7 +6,7 @@ export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
 export function TextInput(props: TextInputProps) {
   return (
     <label className='border border-zinc-400 bg-white rounded px-4 py-2'>
-      <input className='w-72 h-full bg-none outline-none' {...props}/>
+      <input className='w-72 h-full bg-none outline-none placeholder-gray-500' {...props}/>
     </label>
   )
 }
--- app/pages/landing/index.tsx
+++ app/pages/landing/index.tsx
@@ -16,7 +16,7 @@ export default function Landing() {
   return (
     <main className='bg-zinc-200 w-full h-full flex justify-center items-center'>
       <form className='bg-white px-8 py-5 rounded shadow flex flex-col gap-10'>
-        <h1 className='text-lg'>Echoes - Simulador Ausculta</h1>
+        <h1 className='text-lg text-black'>Echoes - Simulador Ausculta</h1>
         <TextInput
           placeholder='E-mail'
           type='email'
