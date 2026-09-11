a4a6dd8925479d6faee49c9f32e9e32be9a4cf63
Avraham <souzaavraham@proton.me>
2026-09-10 22:29:13 -0300

    feat: setup da tabela de usuários

---
+++ app/components/atoms/UserTableRow.tsx
@@ -0,0 +1,45 @@
+import { FiEdit2, FiTrash2 } from "react-icons/fi";
+import type { User } from "~/types/user"
+
+export interface UserTableRowProps {
+  user: User;
+}
+
+export function UserTableRow({ user }: UserTableRowProps) {
+
+  const label = user.role === 'admin' ? 'Administrador' :
+    user.role === 'manager' ? 'Gestor' :
+    user.role === 'teacher' ? 'Professor' :
+    'Estudante'
+
+  return (
+      <tr className='text-black border-t border-b border-gray-800/20'>
+        <td className="w-1/4 px-4 py-2">
+        {user.name}
+        </td>
+
+        <td className="w-1/4 px-4 py-2 border-l border-gray-800/20">
+        {user.email}
+        </td>
+
+        <td className="w-1/4 px-4 py-2 border-l border-gray-800/20">
+        {label}
+        </td>
+
+        <td className="w-1/12 px-4 py-2 border-l border-gray-800/20">
+          <div className='w-full h-full flex flex-row justify-center items-center gap-3'>
+            <button className='cursor-pointer'>
+              <FiEdit2
+              size={24}
+              />
+            </button>
+            <button className='cursor-pointer'>
+              <FiTrash2
+              size={24}
+              />
+            </button>
+          </div>
+        </td>
+      </tr>
+  )
+}
--- app/pages/dashboard/users/index.tsx
+++ app/pages/dashboard/users/index.tsx
@@ -1,16 +1,111 @@
+import { TableHeadColumn } from "~/components/atoms/TableHeadColumn";
+import { UserTableRow } from "~/components/atoms/UserTableRow";
+
 export default function Devices() {
   return (
-    <main className='flex flex-1 justify-center items-center'>
-      <table>
-        <thead className='bg-main rounded'>
+  <main className="flex flex-1 justify-center items-center">
+    <div className="w-full max-w-5xl">
+      {/* Header */}
+      <table className="w-full table-fixed">
+        <thead className="bg-main">
           <tr>
-            <td className='py-2'>Nome</td>
-            <td>E-mail</td>
-            <td>Função</td>
-            <td>Ações</td>
+            <TableHeadColumn className="w-1/4 rounded-tl-md">
+              Nome
+            </TableHeadColumn>
+
+            <TableHeadColumn className="w-1/4">
+              E-mail
+            </TableHeadColumn>
+
+            <TableHeadColumn className="w-1/4">
+              Função
+            </TableHeadColumn>
+
+            <TableHeadColumn className="w-1/12 rounded-tr-md">
+              Ações
+            </TableHeadColumn>
           </tr>
         </thead>
       </table>
-    </main>
+
+      {/* Body */}
+      <div className="max-h-80 overflow-y-auto scrollbar-thin rounded-b-md border border-main/50">
+        <table className="w-full table-fixed">
+          <tbody>
+            <UserTableRow
+              user={{
+                name: 'Nome do usuário',
+                email: 'usuario@email.com',
+                role: 'admin',
+              }}
+            />
+            <UserTableRow
+              user={{
+                name: 'Nome do usuário',
+                email: 'usuario@email.com',
+                role: 'admin',
+              }}
+            />
+            <UserTableRow
+              user={{
+                name: 'Nome do usuário',
+                email: 'usuario@email.com',
+                role: 'admin',
+              }}
+            />
+            <UserTableRow
+              user={{
+                name: 'Nome do usuário',
+                email: 'usuario@email.com',
+                role: 'admin',
+              }}
+            />
+            <UserTableRow
+              user={{
+                name: 'Nome do usuário',
+                email: 'usuario@email.com',
+                role: 'admin',
+              }}
+            />
+            <UserTableRow
+              user={{
+                name: 'Nome do usuário',
+                email: 'usuario@email.com',
+                role: 'admin',
+              }}
+            />
+            <UserTableRow
+              user={{
+                name: 'Nome do usuário',
+                email: 'usuario@email.com',
+                role: 'admin',
+              }}
+            />
+            <UserTableRow
+              user={{
+                name: 'Nome do usuário',
+                email: 'usuario@email.com',
+                role: 'admin',
+              }}
+            />
+            <UserTableRow
+              user={{
+                name: 'Nome do usuário',
+                email: 'usuario@email.com',
+                role: 'admin',
+              }}
+            />
+            <UserTableRow
+              user={{
+                name: 'Nome do usuário',
+                email: 'usuario@email.com',
+                role: 'admin',
+              }}
+            />
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </main>
   )
 }
